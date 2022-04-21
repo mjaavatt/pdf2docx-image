1ca1098dae53c8c497314794417edf4343815ad0
mjaavatt <erlend@mjaavatten.com>
2022-04-21 15:41:59 +0200

    Update pdf2docx-image.sh

--- pdf2docx-image.sh
+++ pdf2docx-image.sh
@@ -8,8 +8,8 @@ fi
 
 tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
 filename=$RANDOM
-echo $tmp_dir
-echo $filename
+#echo $tmp_dir
+#echo $filename
 convert -density 300 -background white -alpha remove -bordercolor black -border 5 "$1" "$tmp_dir/$filename-%03d.png"
 python pdf2docx-image.py $tmp_dir/$filename
 
