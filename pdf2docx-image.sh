de23c8e207dafc7be450023ca118433217a4474a
mjaavatt <erlend@mjaavatten.com>
2024-07-03 12:42:14 +0200

    Fixing some issues with spaces in the filename

--- pdf2docx-image.sh
+++ pdf2docx-image.sh
@@ -2,16 +2,15 @@
 
 if [[ $# -eq 0 ]] ; then
     echo 'Usage'
-    echo $0' input.pdf'
+    echo "$0 input.pdf"
     exit 1
 fi
 
 tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
 filename=$RANDOM
-#echo $tmp_dir
-#echo $filename
+
 convert -density 300 -background white -alpha remove -bordercolor black -border 5 "$1" "$tmp_dir/$filename-%03d.png"
-python pdf2docx-image.py $tmp_dir/$filename
-outputfilename=$(basename -- $1)
+python "$(dirname "${BASH_SOURCE[0]}")/pdf2docx-image.py" "$tmp_dir/$filename"
+outputfilename=$(basename -- "$1")
 mv demo.docx "${outputfilename%.*}.docx"
-rm -rf $tmp_dir
+rm -rf "$tmp_dir"
